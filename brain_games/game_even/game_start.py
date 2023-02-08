ef398ba406992d5cf44669f71a589f7eb8aa9163
anna_petukhova <aniuta.petuhova2013@yandex.ru>
2023-02-09 00:13:27 +0300

    creating game_even

---
+++ brain_games/game_even/game_start.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import prompt
+import random
+from random import randint
+
+
+def even_start():
+    name = prompt.string('May I have your name? ')
+    print(f'''Hello, {name}!
+Answer "yes" if the number is even, otherwise answer "no"''')
+    
+    i = 1
+    while i <= 3:
+        random_number = randint(1, 100)
+        print(f'Question: {random_number}')
+        player_answer = prompt.string('Your answer: ')
+        game_answer = random_number % 2 == 0 and 'yes' or 'no'
+        if player_answer == game_answer:
+            print('Correct!')
+        i += 1
+        if player_answer != game_answer:
+            print(f'''"{player_answer}" is wrong answer ;(. Correct answer was "{game_answer}".
+Let's try again, {name}!''')
+            return
+    print(f'Congratulations, {name}!')
+    
+    
+def main():
+    even_start()
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.game_even.game_start import even_start
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    even_start()
+
+
+if __name__ == '__main__':
+    main()
