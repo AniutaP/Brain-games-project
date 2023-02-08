a142e8f745cae86e9eca2679f1107f331ab1c0bb
anna_petukhova <aniuta.petuhova2013@yandex.ru>
2023-02-08 13:44:33 +0300

    flake8 install

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import prompt
 
-def welcome_user(): 
-    name = prompt.string('May I have your name? ') 
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
-def main(): 
+
+def main():
     welcome_user()
 
-if __name__ =='__main__':
-    main()    
+
+if __name__ == '__main__':
+    main()
